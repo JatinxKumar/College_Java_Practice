3bc77550575ee6ab1854e05f5b416ac84b981176
Jatin Kumar <info.jatink@gmail.com>
2025-09-12 11:00:25 +0530

    Add files via upload

---
+++ Loops/Pascal.java
@@ -0,0 +1,12 @@
+package Patterns;
+
+public class Pascal {
+    for(int i=0;i<5;i++){
+        for(int j=0;j<(5-i);j++){
+            System.out.println(" ");
+        }
+        for(int k=0;k<i;k++){
+            
+        }
+    }
+}
---
+++ Loops/Pyramid1.java
@@ -0,0 +1,16 @@
+package Patterns;
+public class Pyramid1 {
+    public static void main(String[] args) {
+        for(int i=0;i<=5;i++){
+            for (int j = 0; j <= 5-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
+
