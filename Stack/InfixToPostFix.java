07e6135dfbda2403fe6ec8b9ca51db9202b2fb19
Jatin Kumar <info.jatink@gmail.com>
2025-11-11 14:45:04 +0530

    Aded InfixToPostFix.java file

---
+++ Stack/InfixToPostFix.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+
+public class InfixToPostFix {
+    public static String infixToPostfix(String infix) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder postfix = new StringBuilder();
+
+        for (int i = 0; i < infix.length(); i++) {
+            char c = infix.charAt(i);
+
+            if (Character.isDigit(c)) {
+                postfix.append(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix.append(stack.pop());
+                }
+                stack.pop(); // Remove '('
+            } else {
+                while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(c)) {
+                    postfix.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop());
+        }
+
+        return postfix.toString();
+    }
+
+    private static int precedence(char operator) {
+        if (operator == '+' || operator == '-') {
+            return 1;
+        } else if (operator == '*' || operator == '/') {
+            return 2;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String infix = "3+4*2/(1-5)";
+        System.out.println(infixToPostfix(infix));
+    }
+}
